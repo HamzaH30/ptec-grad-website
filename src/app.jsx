0d58f578a2d514d413e982ff8221af3b1c9116dc
Hamza Haque <hamzahaque30@gmail.com>
2023-11-28 13:46:29 -0600

    implement react-router and links

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import MainPage from "./main-page";
+import Home from "./home";
 import Header from "./header";
+import AboutUs from "./about-us";
+import Gallery from "./gallery";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
-    <MainPage />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- src/header.jsx
+++ src/header.jsx
@@ -1,5 +1,21 @@
-function Header() {
-  return <header></header>;
-}
+import { Link } from "react-router-dom";
 
-export default Header;
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about-us">About Us</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
